9d5db2bbc82c2b0f7045746b7898e47074fb2f89
R-AsInRohit <rasinrohit@gmail.com>
2022-08-15 14:10:58 +0530

    renamed appbar file

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'custom_icons_icons.dart';
+import 'package:twitter_ui/custom_icons_icons.dart';
 
 class MyAppbar extends StatelessWidget with PreferredSizeWidget {
   const MyAppbar({Key? key}) : super(key: key);
@@ -9,8 +9,8 @@ class MyAppbar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(      
-      systemOverlayStyle: const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.white,
+      systemOverlayStyle: const SystemUiOverlayStyle(          
+          statusBarColor: Colors.white,
           statusBarIconBrightness: Brightness.dark),
       backgroundColor: Colors.white,
       title: const Icon(
@@ -21,10 +21,11 @@ class MyAppbar extends StatelessWidget with PreferredSizeWidget {
       leading: const Icon(
         Icons.menu,
         color: Colors.blue,
+        size: 30,
       ),
       actions: [
         Container(
-            margin: const EdgeInsets.only(right: 10),
+            margin: const EdgeInsets.only(right: 15),
             child: const Icon(
               CustomIcons.sparkles,
               color: Colors.blue,
