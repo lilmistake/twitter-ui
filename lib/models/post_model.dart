a8b0b3ea17e22da9c46e089aa943d3b9c3453494
R-AsInRohit <rasinrohit@gmail.com>
2022-08-15 15:48:41 +0530

    models

---
+++ lib/models/post_model.dart
@@ -0,0 +1,13 @@
+class Post {
+  final String handle;
+  final String name;
+  final String image;
+  final String profilePicture;
+  final int likes;
+  final int rts;
+  final int comments;
+  final String timeStamp;
+
+  Post(this.handle, this.name, this.image, this.profilePicture, this.likes, this.rts, this.comments,
+      this.timeStamp);
+}
